95b41a9b0b6a98c9e80b41ce94edc3684da2ce89
Jackie Pearce <jackiepearce83@hotmail.com>
2018-07-11 15:27:24 +0100

    Used the code in DatabaseHelper.cs for retrieving Invertebrates data in the SQL database to write code for retrieving birds data for the birds section of the website.

--- MyGardenWildlife2/Helpers/DatabaseHelper.cs
+++ MyGardenWildlife2/Helpers/DatabaseHelper.cs
@@ -21,6 +21,18 @@ namespace MyGardenWildlife2.Helpers
 
         }
 
+        public static List<CategoryModel> GetBirdsData()
+        {
+            var context = new WildlifeContext();
+
+            var section = context.Sections.Where((e) => e.SectionName == "Birds").First();
+
+            var result = section.CategoryList;
+
+            return result;
+
+        }
+
 
         /*Loop through each category.
          *For each category, loop through species

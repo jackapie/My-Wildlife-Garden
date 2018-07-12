e2bdbf92185f3b3a4c9bc44828587c9105c3fda7
Jackie Pearce <jackiepearce83@hotmail.com>
2018-07-12 16:30:16 +0100

    Annotations to the GetSection function

--- MyGardenWildlife2/Helpers/DatabaseHelper.cs
+++ MyGardenWildlife2/Helpers/DatabaseHelper.cs
@@ -9,12 +9,20 @@ namespace MyGardenWildlife2.Helpers
     public class DatabaseHelper
 
     {
+        //SectionModel is the type, GetSection therefore is of the type SectionMode
+        //GetSection takes a parameter that is SectionName, a string.
         public static SectionModel GetSection(string SectionName)
         {
+            //Create an object of type WildlifeContext, assign this to variable "context"
             var context = new WildlifeContext();
 
+            //Within the WildlifeContext object "context", call "Sections", a property within WildlifeContext, returns an object
+            //On this new object, call a Where function, returns an object.  
+            //On this new object, call the First function, returns an object 
+            //Assign this new object to variable "section"
             var section = context.Sections.Where((e) => e.SectionName == SectionName).First();
-
+            
+            //Sets the result of the overall function to be "section", and exits the function.
             return section;
 
         }

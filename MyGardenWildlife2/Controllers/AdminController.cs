25531394378b7b1cfe6be9daf7fbf3fda436d6d9
Jackie Pearce <jackiepearce83@hotmail.com>
2018-07-17 17:38:30 +0100

    Created an AdminController as a first step to enabling online data editing. This contaiins an action function (SectionList) Also created a SectionList view.

---
+++ MyGardenWildlife2/Controllers/AdminController.cs
@@ -0,0 +1,25 @@
+﻿using MyGardenWildlife2.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyGardenWildlife2.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SectionList()
+        {
+            var dbHelper = new DatabaseHelper();
+            var sections = dbHelper.GetSectionList();
+            return View(sections);
+        }
+    }
+}

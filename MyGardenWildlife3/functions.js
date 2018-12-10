cbadd57d1264116de0ceb521dc23fc83b3e111db
Jackie Pearce <jackiepearce83@hotmail.com>
2018-12-10 16:29:33 +0000

    Added SectionEdit View and also an accompanying ajax fn, however, the doc.ready fn needed to be rerun to make this work.

--- MyGardenWildlife3/functions.js
+++ MyGardenWildlife3/functions.js
@@ -15,35 +15,46 @@ function toggleSomething(whatToToggle) {
 }
 
 $(document).ready(function () {
+    //could make this into a function that gets run on page load and then again at end of each ajax operation
+    pageReadyFn();
+});
+
+function pageReadyFn() {
     $('.photoSection p, .speciesList div').hide();
-    $(".headerLinks a").on("click", function () {
-
+    $(".headerLinks a").on("click", function() {
         clickedOnTextToH1(this);
-
         var id = $(this).attr("id");
-
         if (id === "homeHeader") {
-            $.get("/Home/HomePage/", function (data) {
+            $.get("/Home/HomePage/", function(data) {
                 $(".contentSection").html(data);
-                $('.photoSection p, .speciesList div').hide();
+                pageReadyFn();
+                //$('.photoSection p, .speciesList div').hide();
             });
         }
         else if (id !== "homeHeader") {
-            $.get("/Home/Section/" + id, function (data) {
+            $.get("/Home/Section/" + id, function(data) {
                 $(".contentSection").html(data);
-                $('.photoSection p, .speciesList div').hide();
+                pageReadyFn();
+                //$('.photoSection p, .speciesList div').hide();
             });
         }
     });
-
-    $(".toSectionList").on("click", function () {
+    $("#toSectionList").on("click", function() {
         clickedOnTextToH1(this);
-
-        $.get("/Admin/SectionList/", function (data) {
+        $.get("/Admin/SectionList/", function(data) {
             $(".contentSection").html(data);
+            pageReadyFn();
         });
     });
-});
+    $(".sectionEdit a").on("click", function() {
+        clickedOnTextToH1(this);
+        var id = $(this).attr("id");
+        $.get("/Admin/SectionEdit/" + id, function(data) {
+            $(".contentSection").html(data);
+            pageReadyFn();
+        });
+    });
+}
 
 function clickedOnTextToH1(selector) {
     var heading = $(selector).text();
